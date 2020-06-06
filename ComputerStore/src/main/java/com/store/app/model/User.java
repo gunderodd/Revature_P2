a6c91892720e8be7bf3030cc20e9cf9092b4444b
knoxiiw <knoxiiw@gmail.com>
2020-06-05 18:44:17 -0700

    errors uh oh

---
+++ ComputerStore/src/main/java/com/store/app/model/User.java
@@ -0,0 +1,83 @@
+package com.store.app.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Users")
+public class User {
+	
+	@Id
+	@Column(name = "user_id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	@Column(name = "username", unique = true, nullable = false)
+	// regex, username must have a letter
+	private String username;
+	
+	@Column(name = "password", nullable = false)
+	private String password;
+	
+	// 0 - Customer
+	// 1 - Employee
+	// 2 - Administrator
+	@Column(name = "access_level", nullable = false)
+	private int access_level;
+
+	public User(int id, String username, String password, int access_level) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.access_level = access_level;
+	}
+
+	public User() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getAccess_level() {
+		return access_level;
+	}
+
+	public void setAccess_level(int access_level) {
+		this.access_level = access_level;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", access_level=" + access_level
+				+ "]";
+	}
+	
+	
+}
---
+++ ComputerStore/src/main/java/com/store/app/service/UserServiceImpl.java
@@ -0,0 +1,47 @@
+package com.store.app.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.store.app.dao.UserRepo;
+import com.store.app.model.User;
+
+public class UserServiceImpl implements UserService {
+	private UserRepo ur;
+	
+	@Autowired
+	public UserServiceImpl(UserRepo ur) {
+		this.ur = ur;
+	}
+
+	@Override
+	public User createUser(User u) {
+		return ur.save(u);
+	}
+
+	@Override
+	public User getUserById(int id) {
+		return ur.findById(id).get();
+	}
+	
+	@Override
+	public User getUserByUsername(String username) {
+		return ur.findByUsername(username).get();
+	}
+
+	@Override
+	public List<User> getAllUsers() {
+		return ur.findAll();
+	}
+
+	@Override
+	public User updateUser(User u) {
+		return ur.save(u);
+	}
+
+	@Override
+	public void deleteUserById(int id) {
+		ur.deleteById(id);
+	}
+}
