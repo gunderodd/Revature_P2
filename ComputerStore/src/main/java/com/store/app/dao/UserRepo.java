a96be26b5f2ac6a70c6b321b3c57ae8341eef5b2
knoxiiw <knoxiiw@gmail.com>
2020-06-06 20:21:39 -0700

    Adding User Implementation from 5 June

---
+++ ComputerStore/src/main/java/com/store/app/dao/UserRepo.java
@@ -0,0 +1,13 @@
+package com.store.app.dao;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.store.app.model.User;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer> {
+	public Optional<User> findByUsername(String username);
+}
