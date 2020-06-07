67ec4ef8aa8f2b1f37c7c5cb3f4d18f201da6ddf
gunderodd <gunderodd@hotmail.com>
2020-06-07 15:29:42 -0700

    added storecard model

--- ComputerStore/src/main/java/com/store/app/model/User.java
+++ ComputerStore/src/main/java/com/store/app/model/User.java
@@ -5,15 +5,20 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "Users")
 public class User {
 	
+	@OneToOne(mappedBy = "user")
+	private StoreCard storecard;
+	
 	@Id
 	@Column(name = "user_id")
 	@GeneratedValue(strategy = GenerationType.AUTO)
+//	@OneToOne(mappedBy = "storecard")
 	private int id;
 	
 	@Column(name = "username", unique = true, nullable = false)
