ef7153c670222208489217a66c6fce1181e2ecbe
gunderodd <gunderodd@hotmail.com>
2020-06-09 18:57:44 -0700

    updates to angular, working on user home page

--- computer-store-app/src/app/app-routing.module.ts
+++ computer-store-app/src/app/app-routing.module.ts
@@ -2,11 +2,23 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import{CreateUserComponent} from './create-user/create-user.component';
 import{UserListComponent} from './user-list/user-list.component';
+import { CustomerHomeComponent } from './customer-home/customer-home.component';
+import { PurchaseHistoryComponent } from './purchase-history/purchase-history.component';
+import { ReturnItemsComponent } from './return-items/return-items.component';
+import { StorecardComponent } from './storecard/storecard.component';
+import { ShopMainComponent } from './shop-main/shop-main.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 
 const routes: Routes = [
-  {path:"user", component:CreateUserComponent},
-  {path:"users", component:UserListComponent}
+  { path:"user", component:CreateUserComponent},
+  { path:"users", component:UserListComponent},
+  { path: "customerhome", component: CustomerHomeComponent},
+  { path: "purchasehistory", component: PurchaseHistoryComponent},
+  { path: "returnitems", component: ReturnItemsComponent},
+  { path: "storecard", component: StorecardComponent},
+  { path: "shopmain", component: ShopMainComponent},
+  { path: "loginpage", component: LoginPageComponent}
 ];
 
 @NgModule({
