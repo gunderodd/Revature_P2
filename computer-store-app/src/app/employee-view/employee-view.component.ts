afac33fdef2568913c6427a3ec2e06765084329b
garrett-moss <garrettmoss@live.com>
2020-06-11 17:51:20 -0400

    Added some Employee functionality

--- computer-store-app/src/app/employee-view/employee-view.component.ts
+++ computer-store-app/src/app/employee-view/employee-view.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
+import { ProductService } from '../product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee-view',
@@ -6,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-view.component.css']
 })
 export class EmployeeViewComponent implements OnInit {
+  public product: Product;
+  public products:Product[];
+//   public filteredProducts: Product[];
 
-  constructor() { }
+//   actualInputfield = '';
 
-  ngOnInit(): void {
+//   get inputField(){
+//     return this.actualInputfield;
+//   }
+//   set inputField(temp:string){
+//     this.actualInputfield = temp;
+//     this.filteredProducts = this.actualInputfield?
+//         this.performFilter(this.inputField) : this.products;
+// }
+
+// performFilter(filterValue: string): Product[]{
+//   filterValue = filterValue.toLocaleLowerCase();
+
+//   return this.products.filter(
+//     (products:Product)=>
+//     products.name.toLocaleLowerCase().indexOf(filterValue)!== -1
+//   );
+// }
+
+  constructor(private service: ProductService, private router:Router) {
+    // this.filteredProducts = this.products
+   }
+
+   restockOrders(){
+     console.log(this.product.stock)
+     this.service.restockOrders(this.product.stock).subscribe(res=> this.router.navigate(['employeeview']));
+   }
+
+  ngOnInit() {
+    this.service.getAllProducts().subscribe(data=>{
+      this.products = data;
+    });
   }
 
 }
--- computer-store-app/src/app/product.service.ts
+++ computer-store-app/src/app/product.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Product } from './product';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +9,15 @@ import { Product } from './product';
 export class ProductService {
 
   product: Product;
+  private url:string;
 
-  constructor() { }
+  constructor(private http:HttpClient) { 
+    this.url = "http://54.244.36.228:9000/";
+  }
+  public getAllProducts(): Observable<Product[]>{
+    return this.http.get<Product[]>(this.url+"products");
+  }
+  public restockOrders(stock){
+    return this.http.post<Product>(this.url+"restock", stock);
+  }
 }
