2e6c5592648e5bb5ee2946a2896ab3cf64e58a7e
knoxiiw <knoxiiw@gmail.com>
2020-06-12 09:18:41 -0700

    origins

--- ComputerStore/src/main/java/com/store/app/controller/BusinessExceptionController.java
+++ ComputerStore/src/main/java/com/store/app/controller/BusinessExceptionController.java
@@ -3,10 +3,12 @@ package com.store.app.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.store.app.exception.BusinessException;
 
+@CrossOrigin(origins = "*")
 @ControllerAdvice
 public class BusinessExceptionController {
 	
