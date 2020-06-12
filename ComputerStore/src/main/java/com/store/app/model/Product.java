e3f2a1f77208491203a161f2dffb2dec534c9c75
knoxiiw <knoxiiw@gmail.com>
2020-06-12 15:46:21 -0700

    Fixed recursion issue with serialization of objects

--- ComputerStore/src/main/java/com/store/app/model/Product.java
+++ ComputerStore/src/main/java/com/store/app/model/Product.java
@@ -10,6 +10,10 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@JsonIdentityInfo(generator=ObjectIdGenerators.PropertyGenerator.class, property="productId")
 @Entity
 @Table(name = "Product")
 public class Product {
