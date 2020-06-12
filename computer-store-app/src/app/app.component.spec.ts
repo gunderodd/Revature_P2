a31f2b73b1e5cdacde0e58c9dd97e563a78bc801
gunderodd <gunderodd@hotmail.com>
2020-06-12 14:26:51 -0700

    a few more tests, and bug fix

--- computer-store-app/src/app/app.component.spec.ts
+++ computer-store-app/src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'computer-store-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('computer-store-app');
+    expect(app.title).toEqual('Computer Store App');
   });
 
   it('should render title', () => {
