ea45e32296767f66d52fab6f5bfd2487ad2d7168
gunderodd <gunderodd@hotmail.com>
2020-06-12 12:45:11 -0700

    trying to merge changes

--- computer-store-app/src/app/Services/order-product.service.spec.ts
+++ computer-store-app/src/app/Services/order-product.service.spec.ts
@@ -1,12 +1,23 @@
 import { TestBed } from '@angular/core/testing';
 
 import { OrderProductService } from './order-product.service';
+<<<<<<< Updated upstream
+=======
+import { RouterTestingModule } from '@angular/router/testing'
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+>>>>>>> Stashed changes
 
 describe('OrderProductService', () => {
   let service: OrderProductService;
 
   beforeEach(() => {
+<<<<<<< Updated upstream
     TestBed.configureTestingModule({});
+=======
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule, HttpClientTestingModule]
+    });
+>>>>>>> Stashed changes
     service = TestBed.inject(OrderProductService);
   });
 
