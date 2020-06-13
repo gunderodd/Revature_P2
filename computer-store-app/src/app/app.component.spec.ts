9c7f417128c5139807e67abbaf8017e687ac57e3
gunderodd <gunderodd@hotmail.com>
2020-06-12 21:46:29 -0700

    basic shop layout, minor edits

--- computer-store-app/src/app/app.component.spec.ts
+++ computer-store-app/src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('computer-store-app app is running!');
+    expect(compiled.querySelector('H3').textContent).toContain('Welcome to our Computer Store App');
   });
 });
