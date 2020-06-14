fdf5531de0d7603dd4b873c92b1a3811f3610715
Nigel Bishop <nbishop18@gmail.com>
2020-06-14 18:56:51 -0400

    login with access level

--- computer-store-app/src/app/login-page/login-page.component.ts
+++ computer-store-app/src/app/login-page/login-page.component.ts
@@ -10,42 +10,28 @@ import {Router } from '@angular/router';
 })
 export class LoginPageComponent implements OnInit {
   user:User;
-  myuser:User;
 
   constructor(private service:UserService, private router:Router) {
     this.user= new User();
   }
 
   loginUser(){
-    console.log(this.user.username)
-    console.log(this.user.password)
+    
     this.service.loginUser(this.user.username, this.user.password).subscribe(res => {
       if (typeof res === "object") {
         this.service.setCurrentUser(res); 
 
-        if (this.user.username == 'user') {
+        sessionStorage.setItem('user',JSON.stringify(res) );
+        var userJSONobject = JSON.parse(sessionStorage.getItem('user'));
+        var userOBJECT:User = userJSONobject;
+        sessionStorage.setItem('accesslevel',userOBJECT.accessLevel);
+
+        if (userOBJECT.accessLevel == 'cust') {
           this.router.navigate(['customerhome']);
-        } else if(this.user.username == 'admin') {
+        } else if(userOBJECT.accessLevel == 'emp') {
           this.router.navigate(['employeeview']);
         }
 
-        sessionStorage.setItem('username',this.user.username);
-        sessionStorage.setItem('accesslevel',this.user.accessLevel);
-        sessionStorage.setItem('password',this.user.password);
-
-
-        sessionStorage.setItem('user',JSON.stringify(res) );
-        var userJSONobject = JSON.parse(sessionStorage.getItem('user'));
-        console.log(userJSONobject);
-        let userOBJECT:User = userJSONobject;
-        console.log(userOBJECT.id);
-        console.log(userOBJECT.accessLevel);
-        console.log(userOBJECT.username);
-
-
-
-        
-        console.log(res);
       } else if (typeof res === "string") {
         console.log("type is string");
         console.log(res);
@@ -53,53 +39,24 @@ export class LoginPageComponent implements OnInit {
     });
   }
 
-  // how come we're not just storing the entire User object in the session?
-  // Also, if we're going to use SessionStorage, we might want to get rid of the
-  // stuff we have in UserService regarding storing the current user in a field.
-  // Also, maybe this function would be better in the UserService. -WK
+
   isUserLoggedIn() {
-    let user = sessionStorage.getItem('username')
-
     var userJSONobject = JSON.parse(sessionStorage.getItem('user'));
-    // console.log(userJSONobject);
-    var userOBJECT:User = userJSONobject;
-    //  console.log(userOBJECT);
-    
-    // console.log(userOBJECT.id);
-    // console.log(userOBJECT.accessLevel);
-    // console.log(userOBJECT.username);
-
-    // console.log(!(user === null));
-    return !(user === null);
+    return !(userJSONobject === null);
   }
 
   isUserAdmin() {
-    let user = sessionStorage.getItem('username');
-
-    var userJSONobject = JSON.parse(sessionStorage.getItem('user'));
-    // console.log(userJSONobject);
-    var userOBJECT:User = userJSONobject;
-     console.log(userOBJECT);
-    
-    // console.log(userOBJECT.id);
-    // console.log(userOBJECT.accessLevel);
-    // console.log(userOBJECT.username);
-
-    return (user == 'admin');
-
+    var user = sessionStorage.getItem('accesslevel');
+    return (user == 'emp');
   }
 
   isUserCustomer() {
-    let user = sessionStorage.getItem('username')
-    // let myacc = sessionStorage.getItem('accesslevel')
-    // console.log(!(user === null));
-    return (user == 'user');
+    let user = sessionStorage.getItem('accesslevel')
+    return (user == 'cust');
   }
 
   logOut() {
-    // sessionStorage.removeItem('username')
     sessionStorage.clear();
-    // sessionStorage.removeItem('accesslevel')
   }
 
   ngOnInit(): void {
